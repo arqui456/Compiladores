0a13a517bc868c7acedb26f62fe161f2945e6ac4
França Mac Dowell <francamacdowell@gmail.com>
2020-10-21 22:53:43 -0300

    Add git ignore and grammar class

--- primeira_semana/automato.py
+++ primeira_semana/automato.py
@@ -1,60 +1,59 @@
 import string
 
-list_of_spaces = [" ", "\n", "\t"]
-list_of_compound = ["<=", "<>", ">=", "=="]
-list_of_simple = ["<", ">", "=", "+", "-", "*",
-                  "/", "(", ")", ":", ",", ";", ".", "{", "}"]
+from grammar import Grammar
+
 list_of_characters = list(string.ascii_lowercase)
 list_of_digits = list(string.digits)
+
+grammar = Grammar()
 
 file_path = "arquivo_test2.c"
 
 file = open(file_path, 'r')
-file_lines = file.readlines()
 
-list_of_words = []
+with open(file_path, 'r') as file:
 
-for line in file_lines:
-    word = ""
-    i = 0
-  
-    while i < (len(line)):
-        
-        # se leu um espaço
-        if line[i] in list_of_spaces and word != "":
-            list_of_words.append(word)
-            word = ""
-        # se leu um delimitador composto
-        elif i != len(line) - 1 and (line[i] + line[i+1]) in list_of_compound:
-            if word == "":
-               list_of_words.append(line[i] + line[i+1])
-            else: 
-               list_of_words.append(word)
-               word = ""
-               list_of_words.append(line[i] + line[i+1])
-            i = i + 1
-        # se leu um delimitador simples
-        elif line[i] in list_of_simple:
-           
-            if word == "":
-               list_of_words.append(line[i])
-            else: 
-               list_of_words.append(word)
-               word = ""
-               list_of_words.append(line[i])
-        # se leu uma letra
-        elif line[i] in list_of_characters:
-            word = word + line[i]
-        # se leu um número
-        elif line[i] in list_of_digits:
-            word = word + line[i]
-        
-        i = i + 1
+    file_lines = file.readlines()
+
+    list_of_words = []
     
-            
-print(list_of_words)
+    for line in file_lines:
+        word = ""
+        i = 0
         
+        while i < (len(line)):
+            # se leu um espaço
+            if line[i] in grammar.LIST_OF_SPACES and word != "":
+                list_of_words.append(word)
+                word = ""
+                # se leu um delimitador composto
+            elif i != len(line) - 1 and (line[i] + line[i+1]) in grammar.LIST_OF_COMPOUND:
+                
+                if word == "":
+                    list_of_words.append(line[i] + line[i+1])
+                else:
+                    list_of_words.append(word)
+                    word = ""
+                    list_of_words.append(line[i] + line[i+1])
+                i = i + 1
+                
+            # se leu um delimitador simples
+            elif line[i] in grammar.LIST_OF_SIMPLES:
 
-file.close()
+                if word == "":
+                    list_of_words.append(line[i])
+                else:
+                    list_of_words.append(word)
+                    word = ""
+                    list_of_words.append(line[i])
+            # se leu uma letra
+            elif line[i] in list_of_characters:
+                word = word + line[i]
+            
+            # se leu um número
+            elif line[i] in list_of_digits:
+                word = word + line[i]
+                
+            i = i + 1
 
-
+    print(list_of_words)
---
+++ primeira_semana/grammar.py
@@ -0,0 +1,9 @@
+
+class Grammar():
+
+
+  def __init__(self):
+
+    self.LIST_OF_SPACES = [" ", "\n", "\t"]
+    self.LIST_OF_COMPOUND = ["<=", "<>", ">=", "=="]
+    self.LIST_OF_SIMPLES = ["<", ">", "=", "+", "-", "*", "/", "(", ")", ":", ",", ";", ".", "{", "}"]
