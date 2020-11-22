963873a41d5b9035d55d4e2bfc0d475e109e624d
anarchimedes <arquimedes9137@hotmail.com>
2020-11-22 19:59:32 -0300

    extra example added

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from parser import Parser
 import copy
 
 text_input = """
-print(4 ^ 2);
+print(2 <= 3) end
 """
 
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
-tokens_list = copy.copy(tokens)
+token_list = copy.copy(tokens)
 
-for token in tokens_list:
+for token in token_list:
 	print(token)
 
 print("\nINPUT:", text_input)
@@ -18,8 +18,9 @@ print("OUTPUT:")
 
 pg = Parser()
 pg.parse()
-parser = pg.get_parser()
+parser = pg.get_parser(tokens)
 parser.parse(tokens).eval()
+
 
 
 
